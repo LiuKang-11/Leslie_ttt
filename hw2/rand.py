c011a3f86750615d0d05c044ec6d864d2c085f61
Leslie Liu <s9172051212@gmail.com>
2024-09-03 03:27:25 +0000

    modify hw2 by pylint advice

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,10 +1,10 @@
+#This module provides a function to generate a random array using system commands.
 import subprocess
 
-
-def random_array(arr):
+def random_array(arr): #    Fills an array with random integers generated using the system's shuf command.
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
